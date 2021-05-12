fbc92019c1e2f758f39ce1c1ff5be239e61784b6
anji5h <40964308+anji5h@users.noreply.github.com>
2021-05-12 14:15:08 +0545

    initialize

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: { path: path.join(__dirname, "build"), filename: "bundle.js" },
+  mode: process.env.NODE_ENV || "development",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: { contentBase: path.join(__dirname, "src"), compress: true, port: 4000 },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.(scss|css)$/i,
+        use: ["style-loader", "css-loader","sass-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+};
